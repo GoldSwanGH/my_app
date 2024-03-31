4b7643d3051ef412448b6dfa29545935cea85175
goldswan <goldswanru@gmail.com>
2024-04-01 01:11:51 +0300

    part 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   get 'main/index'
   get 'main/help'
   get 'main/contacts'
@@ -10,5 +11,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "main#index"
 end
